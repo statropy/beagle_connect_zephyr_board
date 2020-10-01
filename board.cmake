c00370dc5d0f6029c672a74bb2756cfd703e898b
Erik Larson <erik@statropy.com>
2020-10-01 19:58:21 -0400

    Add UART1

--- board.cmake
+++ board.cmake
@@ -2,6 +2,4 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher_ifnset(misc-flasher)
-
-board_finalize_runner_args(misc-flasher ${BOARD_DIR}/support/cc2538-bsl.py)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
